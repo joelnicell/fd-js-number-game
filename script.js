6c5ed4bbe027e6681d6586d5bb36319031fccbc0
joelnicell <jjnicell@gmail.com>
2025-07-30 10:30:17 +0100

    initial commit.
    Publish repository, boilerplate html added. Some basic instructions for script

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * console.log("hello world") to message in console
+ * 
+ * prompt("hello") to allow users to type
+ * 
+ * alert() to add a pop up on the screen
+ */
+
+alert("Welcome!\n\nClose out this window and press Cmd + opt + C on safari to open the console and get started")
