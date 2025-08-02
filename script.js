53c934003a5b8cedb509d623cd1237408193864e
joelnicell <jjnicell@gmail.com>
2025-08-02 15:59:20 +0100

    Added more messages and user functionality
    Added more descriptive alert messages if a user enters invalid num or too high / too low.
    
    Moved the attempts logic into the game loop so that if a guess is invalid, it doesn't increase number of guesses.

--- script.js
+++ script.js
@@ -2,79 +2,101 @@ const USER_NUMBER_MIN_LIMIT = 1;
 const USER_NUMBER_MAX_LIMIT = 5;
 const MAX_ATTEMPTS = 5;
 
-let ATTEMPTS_NUMBER;
-
 function generateRandomNumber() {
   const randomNumber = Math.ceil(Math.random() * USER_NUMBER_MAX_LIMIT);
 
   return randomNumber;
 }
 
-function getPlayerGuess(
-  message = `guess the evil AI's number! 😈🤖 (between ${USER_NUMBER_MIN_LIMIT} and ${USER_NUMBER_MAX_LIMIT}.)`
-) {
-  if (ATTEMPTS_NUMBER >= MAX_ATTEMPTS) {
-    return;
-  }
+function getPlayerGuess(message = `guess the evil AI's number! 😈🤖 (between ${USER_NUMBER_MIN_LIMIT} and ${USER_NUMBER_MAX_LIMIT}.)`) {
+    const userPrompt = prompt(message);
 
-  const userPrompt = prompt(message);
+    if (userPrompt === null) {
+        alert("You cancelled the game.");
+        return;
+    }
 
-  if (userPrompt === null) {
-    alert("You canceled the game.");
-    return;
-  }
+    const userNumber = Number(userPrompt);
 
-  ATTEMPTS_NUMBER += 1;
-
-  const userNumber = Number(userPrompt);
-
-  if (
-    isNaN(userNumber) ||
-    userNumber < USER_NUMBER_MIN_LIMIT ||
-    userNumber > USER_NUMBER_MAX_LIMIT
-  ) {
-    return getPlayerGuess();
-  }
+    if (isNaN(userNumber)) {
+        alert("Please enter a valid number");
+        return getPlayerGuess();
+    } else if (
+        userNumber < USER_NUMBER_MIN_LIMIT ||
+        userNumber > USER_NUMBER_MAX_LIMIT
+    ) {
+        alert(`Please enter a number between ${USER_NUMBER_MIN_LIMIT} and ${USER_NUMBER_MAX_LIMIT}`)
+        return getPlayerGuess();
+    }
 
   return userNumber;
 }
 
 function checkGuess(userNumber, correctNumber) {
-  if (userNumber > correctNumber) {
-    return "too high";
-  }
+    if (userNumber > correctNumber) {
+        return "high";
+    }
 
-  if (userNumber < correctNumber) {
-    return "too low";
-  }
+    if (userNumber < correctNumber) { 
+        return "low";
+    }
 
   return "correct";
 }
 
 function game() {
-  ATTEMPTS_NUMBER = 0;
+    let ATTEMPTS_NUMBER = 0;
 
-  let checkedGuessMessage;
+    const randomNumber = generateRandomNumber();
 
-  const randomNumber = generateRandomNumber();
+    do {
+        const userNumber = getPlayerGuess();
 
-  do {
-    const userNumber = getPlayerGuess();
+        if (typeof userNumber !== "number") {
+            break;
+        }
 
-    if (typeof userNumber !== "number") {
-      break;
-    }
+        ATTEMPTS_NUMBER += 1;
 
-    checkedGuessMessage = checkGuess(userNumber, randomNumber);
+        const checkedGuess = checkGuess(userNumber, randomNumber);
+        const remainingAttempts = MAX_ATTEMPTS - ATTEMPTS_NUMBER;
 
-    alert(checkedGuessMessage);
+        if (remainingAttempts <= 0) {
+            alert(`Better luck next time! You didn't guess the number correctly. The correct answer was ${randomNumber}\n\nRefresh the page to try again.`)
+            break;
+        }
+        
+        if (checkedGuess === "correct") {
+            alert(`You Win! 👑 The correct answer was ${randomNumber}\n\nYou managed to guess the number in ${ATTEMPTS_NUMBER} attemps. ${generateAttemptsMessage(ATTEMPTS_NUMBER)}\n\nRefresh the page to play again! 👾`);
+            break;
+        } else if (checkedGuess === "high") {
+            alert(`The number you've chosen is too high!\n\n${remainingAttempts} attempts remaining...`)
+        } else {
+            alert(`The number you've chosen is too low!\n\n${remainingAttempts} attempts remaining...`)
+        }
+    } while (ATTEMPTS_NUMBER < MAX_ATTEMPTS);
 
-    if (checkedGuessMessage === "correct") {
-      break;
-    }
-  } while (ATTEMPTS_NUMBER < MAX_ATTEMPTS);
-
-  console.log(`game is ${checkedGuessMessage === "correct" ? "won" : "lost"}`);
-
-  console.log(`attempts used: ${ATTEMPTS_NUMBER}`);
+    console.log("attempts: ", ATTEMPTS_NUMBER);
+    console.log("number: ", randomNumber);
 }
+
+function generateAttemptsMessage(attempts) {
+    switch (attempts) {
+        case 1:
+        case 2:
+        case 3:
+            return "WOW that was super speedy 🔥";
+        case 4:
+        case 5:
+        case 6:
+            return "Good job! 👍🏻";
+        case 7:
+        case 8:
+        case 9:
+            return "Close call, but you made it 🙌🏻";
+        default:
+            return "";
+    }
+}
+
+game();
