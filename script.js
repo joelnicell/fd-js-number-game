5f72bf113ca1303aa126675ebc5e1d61d44eb670
vmihalcea08 <vladut.mihalcea23@gmail.com>
2025-08-02 17:43:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,68 @@
 const USER_NUMBER_MIN_LIMIT = 1;
-const USER_NUMBER_MAX_LIMIT = 100;
-const MAX_ATTEMPTS = 10;
+const USER_NUMBER_MAX_LIMIT = 5;
+const MAX_ATTEMPTS = 5;
 
 let ATTEMPTS_NUMBER;
 
 function generateRandomNumber() {
-    const randomNumber = Math.ceil(Math.random() * USER_NUMBER_MAX_LIMIT);
+  const randomNumber = Math.ceil(Math.random() * USER_NUMBER_MAX_LIMIT);
 
-    return randomNumber;
+  return randomNumber;
 }
 
-function getPlayerGuess(message = `guess the evil AI's number! 😈🤖 (between ${USER_NUMBER_MIN_LIMIT} and ${USER_NUMBER_MAX_LIMIT}.)`) {
-    if (ATTEMPTS_NUMBER >= MAX_ATTEMPTS) {
+function getPlayerGuess(
+  message = `guess the evil AI's number! 😈🤖 (between ${USER_NUMBER_MIN_LIMIT} and ${USER_NUMBER_MAX_LIMIT}.)`
+) {
+  if (ATTEMPTS_NUMBER >= MAX_ATTEMPTS) {
     return;
-    }
+  }
 
-    const userPrompt = prompt(message);
+  const userPrompt = prompt(message);
 
-    if (userPrompt === null) {
-        alert("You canceled the game.");
-        return;
-    }
+  if (userPrompt === null) {
+    alert("You canceled the game.");
+    return;
+  }
 
-    ATTEMPTS_NUMBER += 1;
+  ATTEMPTS_NUMBER += 1;
 
-    const userNumber = Number(userPrompt);
+  const userNumber = Number(userPrompt);
 
-    if (
-        isNaN(userNumber) ||
-        userNumber < USER_NUMBER_MIN_LIMIT ||
-        userNumber > USER_NUMBER_MAX_LIMIT
-    ) {
-        return getPlayerGuess();
-    }
+  if (
+    isNaN(userNumber) ||
+    userNumber < USER_NUMBER_MIN_LIMIT ||
+    userNumber > USER_NUMBER_MAX_LIMIT
+  ) {
+    return getPlayerGuess();
+  }
 
-    return userNumber;
+  return userNumber;
 }
 
 function checkGuess(userNumber, correctNumber) {
-    if (userNumber > correctNumber) {
-        return "too high";
-    }
+  if (userNumber > correctNumber) {
+    return "too high";
+  }
 
-    if (userNumber < correctNumber) {
-        return "too low";
-    }
+  if (userNumber < correctNumber) {
+    return "too low";
+  }
 
-    return "correct";
+  return "correct";
 }
 
 function game() {
-    ATTEMPTS_NUMBER = 0;
-    let checkedGuessMessage;
+  ATTEMPTS_NUMBER = 0;
 
-    const randomNumber = generateRandomNumber();
+  let checkedGuessMessage;
 
-    do {
-        const userNumber = getPlayerGuess();
+  const randomNumber = generateRandomNumber();
 
-        if (typeof userNumber !== "number") {
-        break;
+  do {
+    const userNumber = getPlayerGuess();
+
+    if (typeof userNumber !== "number") {
+      break;
     }
 
     checkedGuessMessage = checkGuess(userNumber, randomNumber);
@@ -67,11 +70,11 @@ function game() {
     alert(checkedGuessMessage);
 
     if (checkedGuessMessage === "correct") {
-        break;
+      break;
     }
   } while (ATTEMPTS_NUMBER < MAX_ATTEMPTS);
 
-    console.log(`game is ${checkedGuessMessage === "correct" ? "won" : "lost"}`);
+  console.log(`game is ${checkedGuessMessage === "correct" ? "won" : "lost"}`);
 
-    console.log(`attempts used: ${ATTEMPTS_NUMBER}`);
+  console.log(`attempts used: ${ATTEMPTS_NUMBER}`);
 }
